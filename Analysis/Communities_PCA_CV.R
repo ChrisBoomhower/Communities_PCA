b0a4c1fa7072159693f0211d154cfe6ac5951815
Chris Boomhower <cboomhower@smu.edu>
2016-10-29 18:42:50 -0500

    Now using training set to build PCR model and using test set to validate model predictions. Added R-squared value outputs for principal component counts.

--- Analysis/Communities_PCA_CV.R
+++ Analysis/Communities_PCA_CV.R
@@ -19,20 +19,36 @@ PCR.out <- function(mymodel){
     validationplot(mymodel) #Plot root mean squared error
     validationplot(mymodel,val.type="MSEP") #Plot mean squared error
     validationplot(mymodel,val.type="R2") #Plot R2
-    predplot(mymodel) #Plot predicted vs. measured values
+    predplot(mymodel,line = TRUE) #Plot predicted vs. measured values
 }
 
-# Commented out because would rather see PRESS values than coefficent of variance (cv) values for cross-validation
-# comm2_pcr_cv <- pcr(ViolentCrimesPerPop~., data = comm2, scale = TRUE, validation = "LOO")#, segments = 10)
-# PCR.out(comm2_pcr_cv)
+# Obtain Root Mean Square Error coefficent of variance (cv) values for leave-one-out cross-validation
+train_pcr_cv <- pcr(ViolentCrimesPerPop~., data = train, scale = TRUE, validation = "LOO")#, segments = 10)
+PCR.out(train_pcr_cv)
 
 # Perform PCR with leave-one-out cross-validation (PRESS statistic) to see how many components PCR identifies as appropriate
-comm2_pcr <- pcr(ViolentCrimesPerPop~., data = comm2, scale = TRUE)
-comm2_cv  <- crossval(comm2_pcr, length.seg = 1) #Cross-validate using leave-one-out
-comm2_cv$validation$PRESS #NOTICE PRESS VALUE DECREASES THROUGH 15 PCs AND THEN RISES AT 16 PCs
-PCR.out(comm2_pcr)
+train_pcr <- pcr(ViolentCrimesPerPop~., data = train, scale = TRUE)
+train_cv  <- crossval(train_pcr, length.seg = 1) #Cross-validate using leave-one-out
+train_cv$validation$PRESS #NOTICE PRESS VALUE DECREASES THROUGH 15 PCs AND THEN RISES AT 16 PCs
+PCR.out(train_pcr)
 #CROSS-VALIDATION PROCEDURE IDENTIFIES 15 PCs AS BEING THE APPROPRIATE NUMBER OF COMPONENTS
 
 # Observe PCR results for first 15 components
-comm2_pcr.ncomp <- pcr(ViolentCrimesPerPop~., data = comm2, ncomp = 15, scale = TRUE, validation = "none")#, segments = 10)
-PCR.out(comm2_pcr.ncomp)
+train_pcr.ncomp <- pcr(ViolentCrimesPerPop~., data = train, ncomp = 15, scale = TRUE, validation = "none")#, segments = 10)
+PCR.out(train_pcr.ncomp)
+R2(train_pcr.ncomp) # Output R^2 (unadjusted) for all PCs
+
+# Observe loadings again, but in plot form
+plot(train_pcr.ncomp, "loadings", comps = 1:2, legendpos = "topright", xlab = "Variables by Number",
+     ylim = c(-0.3,0.3))
+abline(h = 0)#, v = seq(1,88,1))
+
+
+
+
+# Compare predicted root-mean-square error against test set root-mean-square-error
+RMSEP(train_pcr.ncomp, newdata = test)
+
+# Predict ViolentCrimesPerPop using principal components model (currently comparing 7 and 15 PCs)
+formattable(as.data.frame(predict(train_pcr.ncomp, ncomp = c(7,15), newdata = test)))
+predplot(train_pcr.ncomp, ncomp = 15, newdata = test, line = TRUE)
